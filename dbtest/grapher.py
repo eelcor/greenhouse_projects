50ae739c6ca4595cacb7e64e8c74f18aff3e2d86
eelcor <eelco.rouw@gmail.com>
2013-09-30 00:12:59 +0200

    Tidied up and included pressure

--- dbtest/grapher.py
+++ dbtest/grapher.py
@@ -1,6 +1,7 @@
 import sqlite3
 from matplotlib import pyplot
 
+#Connect to the current database and retrieve the time series
 con = sqlite3.connect('tempseries')
 con.text_factory = str
 cur = con.cursor()
@@ -8,7 +9,11 @@ cur.execute("select value from temp_series where event = 'Temperature' ORDER by 
 temp = [r[0] for r in cur.fetchall()]
 cur.execute("select value from temp_series where event = 'Light intensity' ORDER by date ASC")
 light = [s[0] for s in cur.fetchall()]
-pyplot.subplot(211)
+cur.execute("select value from temp_series where event = 'Pressure' ORDER by date ASC")
+pressure = [t[0] for t in cur.fetchall()]
+
+#Plot the temperature
+pyplot.subplot(311)
 pyplot.plot(temp)
 x1,x2,y1,y2 = pyplot.axis()
 print(x1,x2,y1,y2)
@@ -17,10 +22,21 @@ pyplot.grid(True)
 pyplot.axhline(y=0, linestyle='--')
 pyplot.ylabel('Temperature')
 pyplot.xlabel('Sample')
-pyplot.subplot(212)
+
+#Plot the light intensity
+pyplot.subplot(312)
 pyplot.plot(light)
 pyplot.grid(True)
 pyplot.ylabel('Light Intensity')
 pyplot.xlabel('Sample')
+
+#Plot the pressure
+pyplot.subplot(313)
+pyplot.plot(pressure)
+pyplot.grid(True)
+pyplot.ylabel('Pressure')
+pyplot.xlabel('Sample')
+
+#Wrap it up
 pyplot.savefig('simple.png')
 pyplot.show()
