07e3ccc98b10d4aa9619d2e94774206db2b0d321
Eelco Rouw <eelco.rouw@gmail.com>
2013-11-10 11:55:32 +0000

    test for the weather class

---
+++ web_test/test_weather.py
@@ -0,0 +1,10 @@
+import weather
+
+GPIO.setup("P8_10", GPIO.OUT)
+weer = weather()
+while 1:
+	weer.print_climate()
+	GPIO.output("P8_10", GPIO.HIGH)
+	time.sleep(0.1)
+	GPIO.output("P8_10", GPIO.LOW)
+	time.sleep(1)
